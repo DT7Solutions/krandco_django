e0f1e2dcb44f696e4912448123c319e42d2e4bf2
somesetty manideep <developer@dt7solutions.com>
2024-06-12 10:10:42 +0530

    all isuuses fixed

--- krandco/app/admin.py
+++ krandco/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Contact,ProductItem
 from import_export.admin import ImportExportModelAdmin
+from import_export.formats.base_formats import XLSX, CSV, JSON
+
 from .resources import ProductItemResource
 
 # Register your models here.
@@ -13,6 +15,7 @@ admin.site.register(Contact,AdminContact)
 
 class AdminProduct(ImportExportModelAdmin):
     list_display=['Title','Create_at','CreatedName','Type']
+    formats = [XLSX, CSV, JSON]
     resource_class = ProductItemResource
 
 
